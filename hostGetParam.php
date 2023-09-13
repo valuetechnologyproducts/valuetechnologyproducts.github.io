2f7d8947177b3725203640c69546f9b9e9d0326f
valutechnologyproducts <144951747+valutechnologyproducts@users.noreply.github.com>
2023-09-13 15:31:48 -0400

    Add files via upload

---
+++ hostGetParam.php
@@ -0,0 +1,28 @@
+<?php //Get PHP configuration
+////////////////////////////////Attention d'enregistrer ce fichier sous notePad avec un encodage : UTF-8 (sans BOM)////////////////////
+
+
+error_reporting (0);//E_ALL & ~E_NOTICE
+ini_set("display_errors",0);
+
+//Version PHP
+$tabres["PhpVersion"]=phpversion();
+//Chemin physique
+$tabres["RootPath"]=str_replace("hostGetParam.php","",$_SERVER['SCRIPT_FILENAME']);
+//Available PHP modules
+$tabres["AvailablePHPModules"] = implode(';',get_loaded_extensions());
+
+// check index.* to test for obsolete conflicting files
+$tabres["IndexFiles"] = ""; // ex. "index.htm;index.html;index.php"
+$filesIndex = glob("index.*");
+if (!empty($filesIndex)) {
+	foreach ($filesIndex as $filename) {
+		if (in_array($filename, array("index.html", "index.htm", "index.php", "index.asp", "index.aspx")))
+			$tabres["IndexFiles"] .= (($tabres["IndexFiles"]) ? ';' : '').$filename;
+	}
+}
+
+
+echo json_encode($tabres);
+
+?>
